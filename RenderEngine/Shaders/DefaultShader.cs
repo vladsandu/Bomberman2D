b488fcc423824755dde4ca81746335bbb7cdc215
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-16 19:20:02 +0300

    Add DefaultShader shader program

---
+++ RenderEngine/Shaders/DefaultShader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using OpenTK;
+
+namespace RenderEngine.Shaders
+{
+    public class DefaultShader : ShaderProgram
+    {
+
+        private static String VERTEX_FILE = "src/shaders/vertexShader.glsl";
+	    private static String FRAGMENT_FILE = "src/shaders/fragmentShader.glsl";
+	
+	private int location_transformationMatrix;
+        private int location_viewMatrix;
+        private int location_isBackground;
+        private int location_isText;
+        private int location_xOffset;
+        private int location_yOffset;
+
+        public DefaultShader()
+            : base(VERTEX_FILE, FRAGMENT_FILE)
+        {
+            
+        }
+
+        protected void bindAttributes()
+        {
+
+            base.bindAttribute(0, "positions"); //bindam la variabila positions folosita in vertexshader ce se afla in vao-ul curent la atributul 0 adica VBO-ul de pozitii ale vertexurilor
+            base.bindAttribute(1, "textureCoords");
+        }
+
+        protected void getAllUniformLocations()
+        {
+            location_transformationMatrix = base.GetUniformLocation("transformationMatrix");
+            location_viewMatrix = base.GetUniformLocation("viewMatrix");
+            location_isBackground = base.GetUniformLocation("isBackground");
+            location_isText = base.GetUniformLocation("isText");
+            location_xOffset = base.GetUniformLocation("xOffset");
+            location_yOffset = base.GetUniformLocation("yOffset");
+        }
+
+        public void loadXOffset(float value)
+        {
+            base.LoadFloat(location_xOffset, value);
+        }
+
+        public void loadYOffset(float value)
+        {
+            base.LoadFloat(location_yOffset, value);
+        }
+
+        public void loadBackgroundCheck(Boolean value)
+        {
+            base.LoadBoolean(location_isBackground, value);
+        }
+
+        public void loadTextCheck(Boolean value)
+        {
+            base.LoadBoolean(location_isText, value);
+        }
+
+        public void loadTransformationMatrix(Matrix4 matrix)
+        {
+            base.LoadMatrix(location_transformationMatrix, matrix);
+        }
+
+        public void loadViewMatrix(Camera camera)
+        {
+            Matrix4 matrix = Maths.createViewMatrix(camera);
+            LoadMatrix(location_viewMatrix, matrix);
+        }
+
+    }
+}
---
+++ RenderEngine/Shaders/ShaderProgram.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
+
+namespace RenderEngine.Shaders
+{
+    public class ShaderProgram
+    {
+
+        private int programID;
+        private int vertexShaderID;
+        private int fragmentShaderID;
+        
+        public ShaderProgram(String vertexFile, String fragmentFile)
+        {
+
+            this.vertexShaderID = loadShader(vertexFile, ShaderType.VertexShader); //apelam metoda de incarcare si salvam id-ul
+            this.fragmentShaderID = loadShader(fragmentFile, ShaderType.FragmentShader);
+            this.programID = GL.CreateProgram(); //creem programul
+            GL.AttachShader(programID, vertexShaderID);
+            GL.AttachShader(programID, fragmentShaderID); //atasam shaderele programului
+
+            BindAttributes(); //metoda care linkeaza inputurile pentru shadere
+
+            GL.LinkProgram(programID);  //creeaza executabile care vor fi rulate de procesorul grafic
+            GL.ValidateProgram(programID); //valideaza daca s-au creat cum trebuie executabilele
+
+            GetAllUniformLocations(); //sa ia de la bun inceput toate locatiile variabilelor uniforme
+        }
+
+        protected virtual void GetAllUniformLocations()
+        {
+            
+        }
+        //astea sunt daca nu vrem sa bindam la un anume VAO inputurile ci sa. bagam matricile noastre fara sa ne folosim de VAO
+        protected int GetUniformLocation(String uniformName)
+        {
+            return GL.GetUniformLocation(programID, uniformName);   //cauta locatia din codul shaderului unde se afla o variabila cu un anumit nume dintr-un anume program
+        }
+
+        //in loc sa bindam la un atribut din VAO bagam direct valoarea pe care o vrem in inputul din vertex shader
+        protected void LoadFloat(int location, float value)
+        {
+            GL.Uniform1(location, value);
+        }
+
+        protected void LoadInt(int location, int value)
+        {
+            GL.Uniform1(location, value);
+        }
+
+        protected void LoadVector(int location, Vector3 vector)
+        {
+            GL.Uniform3(location, vector.X, vector.Y, vector.Z);
+        }
+
+        protected void LoadBoolean(int location, bool value)
+        {
+            float toLoad = 0;
+            if (value)
+                toLoad = 1;
+
+            GL.Uniform1(location, toLoad);
+        }
+
+        protected void LoadMatrix(int location, Matrix4 matrix)
+        {
+            GL.UniformMatrix4(location, false, ref matrix);
+        }
+
+        public void Start()
+        {
+
+            GL.UseProgram(programID);
+        }
+
+        public void Stop()
+        {
+
+            GL.UseProgram(0);
+        }
+
+        public void CleanUp()
+        {
+
+            Stop();
+            GL.DetachShader(programID, vertexShaderID);
+            GL.DetachShader(programID, fragmentShaderID);
+            GL.DeleteShader(fragmentShaderID);
+            GL.DeleteShader(vertexShaderID);
+            GL.DeleteProgram(programID);
+        }
+
+        protected virtual void BindAttributes()
+        {
+            //metoda care linkeaza inputurile pentru shadere. ex pozitii.
+            //(una din atributele VAO)
+        }
+
+        protected void bindAttribute(int attribute, String variableName)
+        { //bindeaza un atribut la un anumit nume. gen positions
+
+            GL.BindAttribLocation(programID, attribute, variableName); //bindeaza pentru programul ala un atribut la un nume
+
+        }
+
+        private static int loadShader(String file, ShaderType type)
+        {
+
+            //in shaderSource vom construi un string lung cu tot textul din fisierul shaderului. reader fiind obiect de citire a fisierului
+            StringBuilder shaderSource = new StringBuilder();
+
+            try
+            {
+                StreamReader shaderContent = new StreamReader(file);
+                string line;
+                while ((line = shaderContent.ReadLine()) != null)
+                {
+                    shaderSource.Append(line).Append("\n");
+                }
+                shaderContent.Close();
+
+            }
+            catch (IOException e)
+            {
+            }
+
+            //creem un shader cu tipul trimis ca argument, atasam sursa id-ului respectiv, si il compilam
+            int shaderID = GL.CreateShader(type);
+            GL.ShaderSource(shaderID, shaderSource.ToString());
+            GL.CompileShader(shaderID);
+
+//            if (GL.GetShader(shaderID, ShaderParameter.CompileStatus) == GL_FALSE)
+//            {
+//                System.out.println(GL20.glGetShaderInfoLog(shaderID, 500));
+//                System.err.println("Could not compile shader!");
+//                System.exit(-1);
+//            }
+            //erori 
+
+            //returnare de id
+            return shaderID;
+
+
+        }
+
+    }
+}
