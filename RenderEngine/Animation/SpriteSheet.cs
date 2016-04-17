b0ddddd6f1041ae698bbbfc321d6126089614db3
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-17 10:20:45 +0300

    Add class for representing SpriteSheets

---
+++ RenderEngine/Animation/SpriteSheet.cs
@@ -0,0 +1,24 @@
+﻿namespace RenderEngine.Animation
+{
+    public class SpriteSheet
+    {
+        protected int SpriteSizeX { get; set; }
+        protected int SpriteSizeY { get; set; }
+        protected int Rows { get; set; }
+        protected int Cols { get; set; }
+        protected int SpriteNumber { get; set; }
+        protected int ImageWidth { get; set; }
+        protected int ImageHeight { get; set; }
+
+        public SpriteSheet(int spriteSizeX, int spriteSizeY, int rows, int cols, int spriteNumber)
+        {
+            SpriteSizeX = spriteSizeX;
+            SpriteSizeY = spriteSizeY;
+            Rows = rows;
+            Cols = cols;
+            SpriteNumber = spriteNumber;
+            ImageWidth = cols*SpriteSizeX;
+            ImageHeight = rows*SpriteSizeY;
+        }
+    }
+}
