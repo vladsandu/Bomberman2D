56ecd057a2a3a76e942e9c645410af6960219fb5
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-17 14:53:04 +0300

    Add MapRegion class to maps namespace

---
+++ BombermanGame/Maps/MapRegion.cs
@@ -0,0 +1,15 @@
+﻿using OpenTK;
+
+namespace BombermanGame.Maps
+{
+    public class MapRegion
+    {
+        public Vector2 StartCoordinate { get; set; }
+        public int Size { get; set; }
+        public MapRegion(Vector2 startCoordinate, int size)
+        {
+            StartCoordinate = startCoordinate;
+            Size = size;
+        }
+    }
+}
