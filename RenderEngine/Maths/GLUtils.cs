fbde3b610370e1f37e9bf7313f7f9125c1fbec42
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-17 09:59:40 +0300

    Add GLUtils class for common utility methods

---
+++ RenderEngine/Maths/GLUtils.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using OpenTK;
+using RenderEngine.Entities;
+using RenderEngine.Models;
+
+namespace RenderEngine.Maths
+{
+    public class GLUtils
+    {
+        public static Matrix4 createTransformationMatrix(Vector2 translation, float rz, float scale)
+        {
+
+            Matrix4 matrix = Matrix4.Identity;
+            Matrix4.CreateTranslation(translation.X, translation.Y, 0, out matrix);
+            Matrix4.CreateRotationZ(0, out matrix);
+            Matrix4.CreateScale(scale, out matrix);
+
+            return matrix;
+        }
+
+        public static Matrix4 createViewMatrix(Camera camera)
+        {
+            Matrix4 viewMatrix = Matrix4.Identity;
+            Vector3 cameraPos = camera.getPosition();
+            Vector3 negativeCameraPos = new Vector3(-cameraPos.X, -cameraPos.Y, -cameraPos.Z);
+            Matrix4.CreateTranslation(ref negativeCameraPos, out viewMatrix);
+
+            return viewMatrix;
+
+        }
+
+        public static RawModel createQuadPercentTextured(Loader.Loader loader, float widthPercent, float heightPercent,
+                float xTexPercent, float yTexPercent)
+        {
+
+            float actualHeightHalf = heightPercent / 100;
+            float actualWidthHalf = widthPercent / 100;
+
+            float[] vertices ={
+                    -actualWidthHalf, actualHeightHalf, 0.0f,
+                    -actualWidthHalf, -actualHeightHalf, 0.0f,
+                    actualWidthHalf, -actualHeightHalf, 0.0f,
+                    actualWidthHalf, actualHeightHalf, 0.0f,
+            };
+
+            int[] indices = { 0, 1, 3, 3, 1, 2 };
+
+            float[] textureCoords = {
+                0,0,
+                0,1 * yTexPercent/100f,
+                1 * xTexPercent/100f,1 * yTexPercent/100f,
+                1 * xTexPercent/100f,0
+        };
+
+
+            return loader.LoadToVAO(vertices, textureCoords, indices, widthPercent, heightPercent);
+
+        }
+
+        public static RawModel createRectangle(Loader.Loader loader, float widthPercent, float heightPercent)
+        {
+
+            float actualHeightHalf = heightPercent / 100;
+            float actualWidthHalf = widthPercent / 100;
+
+            float[] vertices ={
+                    -actualWidthHalf, actualHeightHalf, 0.0f,
+                    -actualWidthHalf, -actualHeightHalf, 0.0f,
+                    actualWidthHalf, -actualHeightHalf, 0.0f,
+                    actualWidthHalf, actualHeightHalf, 0.0f,
+            };
+
+            int[] indices = { 0, 1, 3, 3, 1, 2 };
+
+            float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+
+            return loader.LoadToVAO(vertices, textureCoords, indices, widthPercent, heightPercent);
+        }
+    }
+}
