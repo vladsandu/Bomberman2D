b6ab257666f362324b434b7536894570c1a1ce61
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-17 10:32:27 +0300

    Add Animation class

---
+++ RenderEngine/Animation/Animation.cs
@@ -0,0 +1,16 @@
+﻿namespace RenderEngine.Animation
+{
+    public class Animation
+    {
+        public SpriteSheet SpriteSheet { get; set; }
+        private int NeededFrameCount { get; set; }
+        private double CurrentFrameCount { get; set; }
+        private int CurrentSpriteNumber { get; set; }
+
+        public Animation(SpriteSheet spriteSheet, int animationSpeedMilis, int fps)
+        {
+            SpriteSheet = spriteSheet;
+            NeededFrameCount = animationSpeedMilis * fps / 1000;
+        }
+    }
+}
