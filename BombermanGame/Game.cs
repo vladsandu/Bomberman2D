77ed528a653b16df88a4f8b6cb448c97c1bb35b2
Vlad Sandu <vladsandu1994@gmail.com>
2016-04-17 09:58:54 +0300

    Move game class to BombermanGame project

---
+++ BombermanGame/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using BombermanGame;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using RenderEngine.Renderer;
+
+namespace RenderEngine
+{
+    public class Game : GameWindow
+    {
+        private MasterRenderer Renderer { get; set; }
+        private CurrentData CurrentData { get; set; }
+        public Game()
+        {
+            Renderer = new MasterRenderer();
+            CurrentData = new CurrentData();
+        }
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            this.VSync = VSyncMode.On;
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            CurrentData.Entity.IncreasePosition(0.01f, 0);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            Renderer.prepare();
+            Renderer.processEntity(CurrentData.Entity);
+            Renderer.render(CurrentData.Camera);
+            SwapBuffers();
+          }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, this.Width, this.Height);
+        }
+    }
+}
