62a75e8940a6434f4068e0772cfe0a1a89d8361d
Vlad Sandu <vlad.sandu@napa.fi>
2016-04-23 11:20:19 +0300

    Add Collision classes to BombermanGame

---
+++ BombermanGame/Collision/CollisionBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using RenderEngine;
+
+namespace BombermanGame.Collision
+{
+    public class CollisionBox
+    {
+        private RectangleF BoundingBox { get; set; }
+
+        public CollisionBox(float x, float y, float width, float height)
+        {
+            float actualHeightHalf = height * Game.GetAspectRatio() / 100f;
+            float actualWidthHalf = width / (100f);
+
+            BoundingBox = new RectangleF(x-actualWidthHalf, y+actualHeightHalf, actualWidthHalf * 2f, actualHeightHalf*2f);
+        }
+
+        public void Move(float x, float y)
+        {
+            BoundingBox.Offset(x, y);
+        }
+
+        public bool Intersects(CollisionBox box)
+        {
+            return BoundingBox.IntersectsWith(box.BoundingBox);
+        }
+    }
+}
---
+++ BombermanGame/Collision/CollisionDetector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BombermanGame.Entities;
+
+namespace BombermanGame.Collision
+{
+    public class CollisionDetector
+    {
+        private CurrentData CurrentData { get; set; }
+
+        public CollisionDetector(CurrentData currentData)
+        {
+            CurrentData = currentData;
+        }
+
+        public bool IsLocalPlayerColliding()
+        {
+            MapRegion currentRegion = GetCurrentMapRegion();
+            if (currentRegion == null)
+                return false;
+            foreach (Tile tile in currentRegion.Tiles)
+            {
+                if (!tile.Walkable && tile.CollisionBox.Intersects(CurrentData.LocalPlayer.CollisionBox))
+                    return true;
+            }
+            return false;
+        }
+
+        private MapRegion GetCurrentMapRegion()
+        {
+            foreach (MapRegion region in CurrentData.Map.Regions)
+            {
+                if (region.Contains(CurrentData.LocalPlayer.Position))
+                    return region;
+            }
+            return null;
+        }
+    }
+}
