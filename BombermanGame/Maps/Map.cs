5f81e2bb089f1ff0f93519b29514b9a6f1ef1f02
Vlad Sandu <vlad.sandu@napa.fi>
2016-04-23 11:21:34 +0300

    Add Map model class

---
+++ BombermanGame/Maps/Map.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BombermanGame.Entities
+{
+    public class Map
+    {
+        public List<MapRegion> Regions { get; set; }
+        
+        public Map()
+        {
+            Regions = new List<MapRegion>();
+        }
+
+        public void AddTile(Tile dirtTile)
+        {
+            foreach (MapRegion region in Regions)
+            {
+                if (region.Contains(dirtTile.Position))
+                {
+                    region.AddTile(dirtTile);
+                    return;
+                }
+            }
+            MapRegion newRegion = new MapRegion(dirtTile.Position, 1000);
+            newRegion.AddTile(dirtTile);
+            Regions.Add(newRegion);
+        }
+    }
+}
--- BombermanGame/Maps/MapLoader.cs
+++ BombermanGame/Maps/MapLoader.cs
@@ -1,4 +1,10 @@
-﻿namespace BombermanGame.Maps
+﻿using System;
+using System.IO;
+using OpenTK;
+using RenderEngine;
+using RenderEngine.Entities;
+
+namespace BombermanGame.Entities
 {
     public class MapLoader
     {
@@ -7,6 +13,56 @@
         public MapLoader(EntityFactory entityFactory)
         {
             EntityFactory = entityFactory;
+        }
+
+        public Map GenerateMap(string filePath, int tilePercentSize)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new IOException("Map: " + filePath + " could not be found.");
+            }
+
+            int xPoz = 0;
+            int yPoz = 0;
+            Map map = new Map();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                //TODO: use NLOG for logging exceptions
+                while (!reader.EndOfStream)
+                {
+                    var position = GetPosition(xPoz, yPoz, tilePercentSize);
+                           
+                    var currentChar = (char) reader.Read();
+                    switch (currentChar)
+                    { 
+                        case '.':
+                            Tile dirtTile = EntityFactory.GetDirtTile(position,
+                                tilePercentSize);
+                            map.AddTile(dirtTile);
+                            xPoz++;
+                            break;
+                        case '\n':
+                            xPoz = 0;
+                            yPoz++;
+                            break;
+                        case ' ':
+                            Tile grassTile = EntityFactory.GetGrassTile(position,
+                                tilePercentSize);
+                            map.AddTile(grassTile);
+                            xPoz++;
+                            break;
+                    }
+                }
+            }
+
+            return map;
+        }
+
+        private Vector2 GetPosition(int xPoz, int yPoz, int tilePercentSize)
+        {
+            return new Vector2(
+                xPoz*(float) tilePercentSize/50f, 
+                -yPoz*(float) tilePercentSize * Game.GetAspectRatio()/50f);
         }
     }
 }
