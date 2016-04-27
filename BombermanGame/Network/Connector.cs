467c76111a46761417038041a96b85a61e85adea
Vlad Sandu <vlad.sandu@napa.fi>
2016-04-27 22:12:56 +0300

    Add networking handling classes for BombermanClient

---
+++ BombermanGame/Network/Connector.cs
@@ -0,0 +1,48 @@
+﻿using BombermanCommons;
+using Mina.Transport.Socket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanGame.Network
+{
+    class Connector : AsyncDatagramConnector
+    {
+        private CurrentData CurrentData { get; set; }
+        public Connector(CurrentData currentData)
+        {
+            CurrentData = currentData;
+
+            ExceptionCaught += (s, e) =>
+            {
+                Console.WriteLine(e.Exception);
+            };
+            MessageReceived += (s, e) =>
+            {
+                Console.WriteLine("Session recv...");
+            };
+            MessageSent += (s, e) =>
+            {
+                Console.WriteLine("Session sent...");
+            };
+            SessionCreated += (s, e) =>
+            {
+                Console.WriteLine("Session created...");
+            };
+            SessionOpened += (s, e) =>
+            {
+                Console.WriteLine("Session opened...");
+            };
+            SessionClosed += (s, e) =>
+            {
+                Console.WriteLine("Session closed...");
+            };
+            SessionIdle += (s, e) =>
+            {
+                Console.WriteLine("Session idle...");
+            };
+        }
+    }
+}
---
+++ BombermanGame/Network/NetHandler.cs
@@ -0,0 +1,51 @@
+﻿using BombermanCommons;
+using BombermanCommons.Network;
+using Mina.Core.Buffer;
+using Mina.Core.Future;
+using Mina.Core.Service;
+using Mina.Core.Session;
+using Mina.Transport.Socket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BombermanGame.Network
+{
+    public class NetHandler
+    {
+        private Connector Connector { get; set; }  
+        private CurrentData CurrentData { get; set; }
+        private IoSession Session { get; set; }
+        public NetHandler(CurrentData currentData)
+        {
+            CurrentData = currentData;
+            Connector = new Connector(currentData);
+        }
+
+        public void Connect()
+        {
+            IConnectFuture connFuture = Connector.Connect(new IPEndPoint(IPAddress.Loopback, 18567));
+            Session = connFuture.Session;
+            connFuture.Await();
+            SendStartSessionMessage();
+        }
+
+        private void SendStartSessionMessage()
+        {
+            SendMessage(new NetMessage(MessageType.START_SESSION, null));
+        }
+
+        public void SendMessage(object obj)
+        {
+            IoBuffer buffer = IoBuffer.Allocate(8);
+            buffer.AutoExpand = true;
+            buffer.PutObject(obj);
+            buffer.Flip();
+            Session.Write(buffer);
+        }
+    }
+}
